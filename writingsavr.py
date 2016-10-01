695fb3a90f7f361f618d6b7886cb6eea3753294d
Caroline <caroline.swinyer@gmail.com>
2016-09-30 20:47:06 -0400

    Changed around some global variables

--- writingsavr.py
+++ writingsavr.py
@@ -1,36 +1,48 @@
 # Save a file to Writing, Dropbox folders
 
-import os, shutil
+import os, shutil, sys
 
-# TODO: Take user input for directories
+# Global variables
+fileInit = 0
+dirInit = 0
 
 # Navigate to folder on hard drive
-os.chdir('C:\\Users\\Caroline\\Documents\\Writing')
+print("Enter a directory to get files from. \n>> ", end="")
+sourceDir = input()
+os.chdir(sourceDir)
 
 # Copies a given file to Dropbox
 def copyToDropbox(file):
-	# TODO: Maybe add a first-time prompt?
-	shutil.copy(filename, 'C:\\Users\\Caroline\\Dropbox\\Fanfiction and Writing')
+	
+	global dirInit
+	
+	if dirInit == 0:
+		print("Where would you like to save this? Enter in the full path name.\n>> ", end="")
+		saveDir = input()
+		dirInit = 1 # One-time prompt
+	
+	shutil.copy(filename, saveDir)
 	print("\n\x1b[1;32mSuccessfully copied\x1b[0m " + filename + " \x1b[1;32mto Dropbox.\x1b[0m\n")
 
-firstHit = 0
-	
-print("Enter anything, or hit ENTER to quit: \n>> ", end="")
+print("Press any key, or hit ENTER to quit: \n>> ", end="")
 while input() != "":
 
-	# If firstHit == 0, ask for filename
-	if firstHit == 0:
+	# If fileInit == 0, ask for filename
+	if fileInit == 0:
 		print("What file would you like to save to Dropbox? \n>> ", end="")
 		saveFile = input() # Regex to search for containment?
-		firstHit = 1 # Prevents return
+		print("Okay, you've entered in \x1b[1;32m" + saveFile + "\x1b[0m.\n")
+		fileInit = 1 # Prevents return
 
+	print("\nBeginning to search directory...")
 	for filename in os.listdir():
 		
-		if filename.startswith('TEST'):
+		#if filename.startswith('TEST'):
+		if filename.find(saveFile) != -1: # Found a match
 			print("\x1b[1;32mFILE: \x1b[0m" + filename) # Linux-specific
 			copyToDropbox(filename)
 			break
 		else:
 			print("FILE: " + filename)
 			
-	print("Enter anything, or hit ENTER to quit: \n>> ", end="")
+	print("Press any key, or hit ENTER to quit: \n>> ", end="")
